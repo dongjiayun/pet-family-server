8d307dddfb85063e2ad47b5787bf8a1722e6a3ee
DONGJIAYUN932 <dongjiayun932@pingan.com.cn>
2023-11-21 11:47:44 +0800

    去除options除了

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"go-pet-family/models"
 	"go-pet-family/router"
 	"go-pet-family/utils"
-	"net/http"
 )
 
 func init() {
@@ -15,12 +13,5 @@ func init() {
 
 func main() {
 	r := router.GetRouter()
-	r.Use(func(c *gin.Context) {
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusOK)
-			return
-		}
-		c.Next()
-	})
 	r.Run("0.0.0.0:2000")
 }
