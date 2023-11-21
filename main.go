3a10205c24ff6450df413123f81001521e3706eb
DONGJIAYUN932 <dongjiayun932@pingan.com.cn>
2023-11-21 11:44:24 +0800

    处理所有options请求

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"go-pet-family/models"
 	"go-pet-family/router"
 	"go-pet-family/utils"
+	"net/http"
 )
 
 func init() {
@@ -13,5 +15,12 @@ func init() {
 
 func main() {
 	r := router.GetRouter()
+	r.Use(func(c *gin.Context) {
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+		c.Next()
+	})
 	r.Run("0.0.0.0:2000")
 }
