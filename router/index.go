11a30ff7f8e21066fe9212e7654b3d2ed2e2aaed
DONGJIAYUN932 <dongjiayun932@pingan.com.cn>
2023-11-21 18:05:30 +0800

    cros

--- router/index.go
+++ router/index.go
@@ -18,7 +18,7 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://club.meowmeowmeow.cn")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusOK)
