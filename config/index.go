512372da2d07cdc279d939941672fa3f49c0cd89
DONGJIAYUN932 <dongjiayun932@pingan.com.cn>
2025-08-16 20:38:42 +0800

    UPDATE DATABASE

--- config/index.go
+++ config/index.go
@@ -4,7 +4,7 @@ import "time"
 
 const Secret = "123456"
 
-const DataBase = "root:@tcp(127.0.0.1:3306)/pet-family?charset=utf8mb4&parseTime=True&loc=Local"
+const DataBase = "root:Djydjydjy9418.@tcp(127.0.0.1:3306)/blog?charset=utf8mb4&parseTime=True&loc=Local"
 
 //const DataBase = "root:@tcp(1.94.65.197:3306)/pet-family?charset=utf8mb4&parseTime=True&loc=Local"
 
